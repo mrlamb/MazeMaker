2a77ad00fb00b1535086858d4fb33c758db842dc
Matthew Lamb <lamb.matthew@gmail.com>
2015-09-18 21:55:23 -0500

    Initial Commit, basic maze cell type declarations.

---
+++ MazeCell/MazeCell.cs
@@ -0,0 +1,19 @@
+﻿namespace MRLamb.MazeMaker
+{
+    public class MazeCell
+    {
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+        public bool InMaze { get; set; }
+        struct Walls
+        {
+            public bool North { get; set; }
+            public bool East { get; set; }
+            public bool South { get; set; }
+            public bool West { get; set; }
+        }
+        public bool Start { get; set; }
+        public bool End { get; set; }
+
+    }
+}
